b6f792551c3296f24794fd2c84a4869dc6015762
cypryy2002 <cypryy2002@yahoo.com>
2022-03-27 21:31:53 -0500

    Initial the first commit

---
+++ src/TestareG.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+public class TestareG {
+    @Test
+    public void test1()
+    {
+        System.out.println("Am uitat o gramada!!");
+    }
+    @Test
+    public void test2()
+    {
+        System.out.println("E scarry");
+    }
+
+}
