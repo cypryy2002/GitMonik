c728f0e673031025bc0fcecbf9ca1038463bda4f
cypryy2002 <cypryy2002@yahoo.com>
2022-03-28 23:08:26 -0500

    Am modificat ceva

--- src/TestareG.java
+++ src/TestareG.java
@@ -4,12 +4,12 @@ public class TestareG {
     @Test
     public void test1()
     {
-        System.out.println("Am uitat o gramada!!");
+        System.out.println("Am uitat aproape tot!!");
     }
     @Test
     public void test2()
     {
-        System.out.println("E scarry");
+        System.out.println("Asta e");
     }
 
 }
